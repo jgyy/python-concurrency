0be1a46a93c4ad2690315e77bf9a87650b0ca544
jeffrey_goh <jeffrey_goh@tech.gov.sg>
2019-08-06 10:16:49 +0800

    updated threading without parenthesiss

--- test_thumbnail_maker.py
+++ test_thumbnail_maker.py
@@ -4,23 +4,27 @@ thumnbail maker script, may change the source
 from thumbnail_maker import ThumbnailMakerService
 
 IMG_URLS = [
-    'https://i.kinja-img.com/gawker-media/image/upload/s--E9JvR6zD--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ycikkm50timeaevel40l.jpg',
-    'https://i.kinja-img.com/gawker-media/image/upload/s--ROdOrqxj--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/muotybwrl9sm4s2jgoig.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--jHAkq6mQ--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ccwlwicp2umrn1dnp2h1.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--7qH7m17g--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ofqe9ruxjn04komwar9o.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--ZelFOYrg--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/vdx7igcimjfcqhe1yfwy.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--L_vCdQkY--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/zwxmqlcov5otterd3837.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--3lZHtAvN--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/alscw3vhlcfzdkzfnz3e.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--4RWC_Q8y--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/juiqfgnxsnd8hztashdj.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--53FOkEHQ--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/rwsm1niblyxtdve1yuvq.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--8oh2GJ8---/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/d4mwn9uy4ednf1nyt44d.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--BNU4s31Q--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/r2fxeggcuapki9ze86g5.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--E9JvR6zD--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ycikkm50timeaevel40l.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--gXbd2e1V--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/jvp8ftnuptptzrkbmwwy.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--qXHAzcVS--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ant9bupe0imzmc7em6za.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--hVbrpwjH--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/k9jzpxlvcccaz4mzmwxt.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--Ie8pBtnS--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/zdux9lg2pierc8t6weay.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--mV5iGurd--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ywooldudzdkem6blteiw.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--pSC41gMP--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/buulcxrypashtjuqkisc.jpg',
-    'https://i.kinja-img.com/gawker-media/image/upload/s--3lZHtAvN--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/alscw3vhlcfzdkzfnz3e.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--ROdOrqxj--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/muotybwrl9sm4s2jgoig.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--TqpTY9t6--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/or8j0njxkotoo0sve2jy.jpg',
-    'https://i.kinja-img.com/gawker-media/image/upload/s--hVbrpwjH--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/k9jzpxlvcccaz4mzmwxt.jpg',
-    'https://i.kinja-img.com/gawker-media/image/upload/s--8oh2GJ8---/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/d4mwn9uy4ednf1nyt44d.jpg',
-    'https://i.kinja-img.com/gawker-media/image/upload/s--gXbd2e1V--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/jvp8ftnuptptzrkbmwwy.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--RPPFz0Qr--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/vteo9oglifm6uykzi90a.jpg',
-    'https://i.kinja-img.com/gawker-media/image/upload/s--FHzgs724--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/thok0ygjqbbucnocsb0h.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--FgqaCeME--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ajs0pcmeh4ollbnt5gxw.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--FHzgs724--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/thok0ygjqbbucnocsb0h.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--XhHj4A_x--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/dee608xivehmprkchnhx.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--C3g1bWMZ--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/rfn9ykjpcnaxuyfqwzf9.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--yR0DdAoE--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/m7vmbgzimblv4su9m2kb.jpg',
@@ -43,8 +47,8 @@ IMG_URLS = [
     'https://i.kinja-img.com/gawker-media/image/upload/s--ho5zOxOS--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/onx5yksjnr8ulmchw1nw.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--HEK5Si0z--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/s4vo17kcic5nctl9qwk6.jpg',
     'https://i.kinja-img.com/gawker-media/image/upload/s--sJJDHlcS--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/wsvwtlwjp62efu20dxbb.jpg',
-    'https://i.kinja-img.com/gawker-media/image/upload/s--sZs0RU_X--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ympzuvjshxjfoen6ax3h.jpg',
-    'https://i.kinja-img.com/gawker-media/image/upload/s--FACPTWC0--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/zeumriyb2xp29m2akvcx.jpg'
+    'https://i.kinja-img.com/gawker-media/image/upload/s--FACPTWC0--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/zeumriyb2xp29m2akvcx.jpg',
+    'https://i.kinja-img.com/gawker-media/image/upload/s--sZs0RU_X--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_1600/ympzuvjshxjfoen6ax3h.jpg'
 ]
 
 
--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -8,7 +8,10 @@ import logging
 from urllib.parse import urlparse
 # noinspection PyCompatibility
 from urllib.request import urlretrieve
+# noinspection PyCompatibility
+from queue import Queue, Empty
 import threading
+from threading import Thread
 
 import PIL
 from PIL import Image
@@ -25,25 +28,30 @@ class ThumbnailMakerService:
         self.home_dir = home_dir
         self.input_dir = self.home_dir + os.path.sep + 'incoming'
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
-        self.downloaded_bytes = 0
         self.dl_lock = threading.Lock()
         max_concurrent_dl = 4
         self.dl_sem = threading.Semaphore(max_concurrent_dl)
+        self.img_queue = Queue()
+        self.dl_queue = Queue()
 
-    def download_image(self, url):
+    def download_image(self):
         """
-        download each image and save to the input dir
-        :param url: the link for each image
+        Download each image and save to the input dir
         """
-        with self.dl_sem:
-            logp("downloading image at URL " + url)
-            img_filename = urlparse(url).path.split('/')[-1]
-            dest_path = self.input_dir + os.path.sep + img_filename
-            urlretrieve(url, self.input_dir + os.path.sep + img_filename)
-            img_size = os.path.getsize(dest_path)
-            with self.dl_lock:
-                self.downloaded_bytes += img_size
-            logp("image [{size} bytes] saved to {path}".format(size=img_size, path=dest_path))
+        while not self.dl_queue.empty():
+            try:
+                url = self.dl_queue.get(block=False)
+                logp("Downloading image at URL " + url)
+                img_filename = urlparse(url).path.split('/')[-1]
+                dest_path = self.input_dir + os.path.sep + img_filename
+                urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+                img_size = os.path.getsize(dest_path)
+                self.img_queue.put(img_filename)
+                logp("Image [{size} bytes] saved to {path}.".format(size=img_size, path=dest_path))
+
+                self.dl_queue.task_done()
+            except Empty:
+                logp("Queue empty")
 
     def download_images(self, img_url_list):
         """
@@ -58,16 +66,20 @@ class ThumbnailMakerService:
         logp("beginning image downloads")
         start = time.perf_counter()
 
-        threads = []
         for url in img_url_list:
-            thr = threading.Thread(target=self.download_image(url))
-            thr.start()
-            threads.append(thr)
-        for thre in threads:
-            thre.join()
-
+            # Download each image and save to the input dir
+            logp("Downloading image at URL " + url)
+            img_filename = urlparse(url).path.split('/')[-1]
+            dest_path = self.input_dir + os.path.sep + img_filename
+            urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+            img_size = os.path.getsize(dest_path)
+            self.img_queue.put(img_filename)
+            logp("Image [{size} bytes] saved to {path}.".format(size=img_size, path=dest_path))
         end = time.perf_counter()
-        logp("downloaded {img:d} images in {time:f} seconds".format(
+
+        self.img_queue.put(None)
+
+        logp("Downloaded {img:d} images in {time:f} seconds.".format(
             img=len(img_url_list), time=end - start
         ))
 
@@ -76,36 +88,40 @@ class ThumbnailMakerService:
         validate inputs
         :return: returns nothing if no input
         """
-        if not os.listdir(self.input_dir):
-            return
         os.makedirs(self.output_dir, exist_ok=True)
 
-        logp("beginning image resizing")
+        logp("Beginning image resizing.")
         target_sizes = [32, 64, 128, 256]
         num_images = len(os.listdir(self.input_dir))
 
         start = time.perf_counter()
-        for filename in os.listdir(self.input_dir):
-            orig_img = Image.open(self.input_dir + os.path.sep + filename)
-            print(filename)
-            for basewidth in target_sizes:
-                print(basewidth)
-                img = orig_img
-                # calculate target height of the resized image to maintain the aspect ratio
-                wpercent = (basewidth / float(img.size[0]))
-                hsize = int((float(img.size[1]) * float(wpercent)))
-                # perform resizing
-                img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
+        while True:
+            filename = self.img_queue.get()
+            if filename:
+                logp("Resizing image {file}.".format(file=filename))
+                orig_img = Image.open(self.input_dir + os.path.sep + filename)
+                for basewidth in target_sizes:
+                    img = orig_img
+                    # calculate target height of the resized image to maintain the aspect ratio
+                    wpercent = (basewidth / float(img.size[0]))
+                    hsize = int((float(img.size[1]) * float(wpercent)))
+                    # perform resizing
+                    img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
 
-                # save the resized image to the output dir with a modified file name
-                new_filename = os.path.splitext(filename)[0] + \
-                    '_' + str(basewidth) + os.path.splitext(filename)[1]
-                img.save(self.output_dir + os.path.sep + new_filename)
+                    # save the resized image to the output dir with a modified file name
+                    new_filename = os.path.splitext(filename)[0] + \
+                        '_' + str(basewidth) + os.path.splitext(filename)[1]
+                    img.save(self.output_dir + os.path.sep + new_filename)
 
-            os.remove(self.input_dir + os.path.sep + filename)
+                os.remove(self.input_dir + os.path.sep + filename)
+                logp("Done resizing image {file}.".format(file=filename))
+                self.img_queue.task_done()
+            else:
+                self.img_queue.task_done()
+                break
+
         end = time.perf_counter()
-
-        logp("created {img:d} thumbnails in {time:f} seconds".format(
+        logp("Created {img:d} thumbnails in {time:f} seconds.".format(
             img=num_images, time=end - start
         ))
 
@@ -116,8 +132,20 @@ class ThumbnailMakerService:
         logp("START make_thumbnails")
         start = time.perf_counter()
 
-        self.download_images(img_url_list)
-        self.perform_resizing()
+        for img_url in img_url_list:
+            self.dl_queue.put(img_url)
+
+        num_dl_threads = 4
+        for _ in range(num_dl_threads):
+            thr = Thread(target=self.download_image)
+            thr.start()
+
+        t_2 = Thread(target=self.perform_resizing)
+        t_2.start()
+
+        self.dl_queue.join()
+        self.img_queue.put(None)
+        t_2.join()
 
         end = time.perf_counter()
         logp("END make_thumbnails in {time:f} seconds".format(time=end - start))
